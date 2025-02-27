1ca7e576a4afc1390e3ea9818146cc949e0e4ed9
Pacolas <160373515+Pacolas@users.noreply.github.com>
2025-02-26 22:19:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@
             
             div.classList.add("mb-4");
         
-            
+            console.log(item.link)
 
             div.innerHTML = `
                 <h4>${item.name}</h4>
